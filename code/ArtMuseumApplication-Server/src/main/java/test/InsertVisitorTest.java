dc47edef6caf36261443c4dca9a410851faeb872
Ana Rednic <37181992+anarednic@users.noreply.github.com>
2018-05-20 21:04:14 +0300

    JUnit test case added

---
+++ code/ArtMuseumApplication-Server/src/main/java/test/InsertVisitorTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import org.junit.Test;
+
+import businessLogic.VisitorBusinessLogic;
+import entity.Visitor;
+public class InsertVisitorTest {
+
+	@Test
+	public void test() {
+		VisitorBusinessLogic vlogic=new VisitorBusinessLogic();
+		List<Visitor> listV=vlogic.viewAllVisitors();
+		int currentNumber=listV.size();
+		Visitor v=new Visitor();
+		v.setName("Test");
+		v.setUsername("test");
+		v.setPassword("test");
+		vlogic.insertVisitor(v);
+		List<Visitor> newlistV=vlogic.viewAllVisitors();
+		int newNumber=newlistV.size();
+		assertEquals(true,newNumber==currentNumber+1);
+		//fail("Insertion failed");
+	}
+
+}
